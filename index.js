208105c696eff39194d0ae8f4ac17b2d9911c76b
PeterMc48 <peter.a.mccafferty@students.ittralee.ie>
2021-05-04 09:27:47 +0100

    Update index.js
    removed swagger

--- index.js
+++ index.js
@@ -4,32 +4,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 const PORT = 4000;
 const db = process.env.DB_connect;
-const swaggerUi = require("swagger-ui-express");
-const swaggerJsdoc = require("swagger-jsdoc");
-
-const options = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Login',
-            version: '1.0.0',
-            description: "A Simple register and login API"
-            },
-    
-        servers: [
-            {
-                url:"http://localhost:4000"
-            }
-        ],
-    },
-    apis: ['./routes/auth.js'],
-};
-
-const openapiSpecification = swaggerJsdoc(options);
 
 const app = express();
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 //routes
 const authRoute = require('./routes/auth');
 
